7491b2b8285d5917cdfb7e93cfd7096160b3cb4d
Oleksandr <alexbos1980@gmail.com>
2023-08-27 17:29:02 +0300

    UC#10 Implement regex

---
+++ app/src/main/java/com/example/regexutility/StringExtension.kt
@@ -0,0 +1,10 @@
+package com.example.regexutility
+
+
+
+fun String.isValidPassword(maxLength: Int): Boolean {
+    val passwordCheckRegex = Regex("^.*(?=.{4," + maxLength +"})(?=.*\\d)(?=.*[a-z])" +
+            "(?=.*[A-Z])(?!.*\\s)(?=.*[!@#\$%^&*()_+|~\\-=\\\\`{}\\[\\]:\\\";'<>?,.\\/]).*\$")
+    return passwordCheckRegex.matches(this)
+}
+
